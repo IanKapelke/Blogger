17f2d6274cf59ddbd919b21c8472c5f6d84dcd21
IanKapelke <ijkapelke@gmail.com>
2017-02-25 17:44:20 -0600

    added functionality to create new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,27 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
   def index
     @articles = Article.all
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    #article_params is helper function
+    @article = Article.new(article_params)
+
+# can alternatively be done this way
+#    @article = Article.new
+#    @article.title = params[:article][:title]
+#    @article.body = params[:article][:body]
+
+    @article.save
+    redirect_to article_path(@article)
   end
 end
